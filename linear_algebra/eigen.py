d5f83eabfdc0721e52bb225ebe3b6cf9dce7a2f4
trong <devgruxcon@gmail.com>
2018-04-02 00:54:24 -0400

    Positive definite matrix

--- linear_algebra/eigen.py
+++ linear_algebra/eigen.py
@@ -1,16 +1,28 @@
 import numpy as np
+from numpy import linalg as LA
 
-A = np.array([[1, 2, 3], [3, 2, 1], [1, 0, -1]])
-w, v = np.linalg.eig(A)
-print(w)  # eigenvalues
-print(v)  # eigenvectors
 
-l, u = w[0], v[:, 0]
-print(np.dot(A, u))
-print(l * u)
-assert np.allclose(np.dot(A, u), l * u), "u is an eigenvector and l is the corresponding eigenvalue"
+def is_positive_definite(A):
+    return np.all(LA.eigvals(A) > 0)
 
-A = np.random.rand(3, 3)
-w, v = np.linalg.eig(A)
-print(w)
-print(v)
+
+def main():
+    A = np.array([[1, 2, 3], [3, 2, 1], [1, 0, -1]])
+    w, v = LA.eig(A)
+    print(w)  # eigenvalues
+    print(v)  # eigenvectors
+
+    l, u = w[0], v[:, 0]
+    print(np.dot(A, u))
+    print(l * u)
+    assert np.allclose(np.dot(A, u), l * u), "u is an eigenvector and l is the corresponding eigenvalue"
+
+    A = np.random.rand(3, 3)
+    w, v = LA.eig(A)
+    print("Eigenvalues of A", w)
+    print("Is A positive definite?", is_positive_definite(A))
+    print(v)
+
+
+if __name__ == "__main__":
+    main()
